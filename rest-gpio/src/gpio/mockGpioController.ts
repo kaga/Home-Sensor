1e95d413db546be6efec6af50f9c86bb43eeca2a
kaga <kaga21@gmail.com>
2016-08-21 14:05:13 +1200

    add missing mockGpioController.ts

---
+++ rest-gpio/src/gpio/mockGpioController.ts
@@ -0,0 +1,28 @@
+import { GpioController, GpioState, GpioConfiguration } from './gpioController';
+import { Promise, resolve } from 'when';
+
+export class MockGpioController implements GpioController {
+    bcmPinNumber: number = 0;
+    state: boolean = false;
+
+    constructor(config?: GpioConfiguration) {
+        if (config) {
+            this.bcmPinNumber = config.bcmPinNumber;
+            this.writeToPin(config.state);
+        }        
+    }
+
+    readPin(): Promise<GpioState> {        
+        return resolve({
+            timestamp: new Date(),
+            bcmPinNumber: this.bcmPinNumber,
+            state: this.state
+        })
+            .tap((state) => console.log(JSON.stringify(state, null, 4)));
+    }
+
+    writeToPin(newState: boolean): Promise<GpioState> {
+        this.state = newState;
+        return this.readPin();
+    }
+}
